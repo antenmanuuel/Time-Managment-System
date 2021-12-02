5d2a732932f49ddfd2196096258ddd0ad5b96500
Ca'Marai Clyburn <c.jordyn.clyburn@gmail.com>
2021-12-01 21:20:21 -0500

    main

--- Main.sh
+++ Main.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
-. functions.sh
+
 
 # Important links for functions:
 # https://linuxize.com/post/bash-functions/
 
 while true; do
-echo ""
-echo "Enter 1 to show today's classes";
-echo ""
-echo "Enter 2 to show assignments that are due today";
-echo  ""
-echo  "Enter 3 to show all incomplete assignments";
-echo ""
-echo "Enter 4 to add an assignment"
-echo ""
-echo "Enter 5 to update an assignment"
-echo -n "Please enter 1-5 or enter anything else to exit."
+echo "
+Enter 1 to display today's classes
+Enter 2 to display assignments that are due today
+Enter 3 to display all incomplete assignments
+Enter 4 to add an assignment
+Enter 5 to update an assignment
+"
+echo -n "Please enter "1-5" or enter "6" to exit: "
+
+
 read choice;
 case "$choice" in
+
         1)
-                echo "$(ifconfig -a | egrep 'inet\s')"
-                echo ""
-		# Calls the 'hello_world' function from functions.sh
-		hello_world
+                echo " "
+		date
+		echo ""
                 ;;
         2)
-                echo "$(who)"
+                echo " "
                 echo ""
                 ;;
         3)
@@ -43,7 +42,7 @@ case "$choice" in
 		;;
 
         *)
-                echo "Exit"
+                echo "Have a good day! Goodby,."
                 exit 1
 
 esac
