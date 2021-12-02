ad884a4ceb32d94392429fb0b97bc195408b3b76
Ca'Marai Clyburn <c.jordyn.clyburn@gmail.com>
2021-12-02 15:18:22 -0500

    main

---
+++ main.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+
+# Important links for functions:
+# https://linuxize.com/post/bash-functions/
+
+while true; do
+echo "
+Enter 1 to display today's classes
+Enter 2 to display assignments that are due today
+Enter 3 to display all incomplete assignments
+Enter 4 to add an assignment
+Enter 5 to update an assignment
+"
+echo -n "Please enter "1-5" or enter "6" to exit: "
+
+
+read choice;
+case "$choice" in
+
+        1)
+                echo " "
+		./classSchedule.sh
+                ;;
+        2)
+                echo " "
+                echo ""
+                ;;
+        3)
+                echo "$(date)"
+                echo ""
+                ;;
+	4)
+		echo "$()"
+		echo ""
+		;;
+	5)
+		echo "$()"
+		echo ""
+		;;
+
+        *)
+                echo "Have a good day! Goodby,."
+                exit 1
+
+esac
+done
+
